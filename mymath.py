5f79e44d18aefb110fe4f8391eadc4094a336015
tranvanquyetn <48422559+tranvanquyetn@users.noreply.github.com>
2019-05-19 22:31:39 +0700

    Add files via upload

---
+++ mymath.py
@@ -0,0 +1,10 @@
+def square (n):
+    return n*n
+def cube (n):
+    return n*n*n
+def average (values):
+    nvals = len(values)
+    sum=0.0
+    for c in values:
+        sum+=c
+        return float (sum)/nvals
